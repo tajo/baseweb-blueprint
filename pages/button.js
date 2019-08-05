45ad1b310b39c2742b16202df851e1549f173d7b
Vojtech Miksu <vojtech@uber.com>
2019-08-05 14:39:14 +0200

    add button enhancers

--- pages/button.js
+++ pages/button.js
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { Button, KIND, SIZE } from "baseui/button";
+import Upload from "baseui/icon/upload";
+import ArrowRight from "baseui/icon/arrow-right";
 
 export default () => (
   <React.Fragment>
@@ -12,5 +14,9 @@ export default () => (
     <Button disabled>Disabled</Button>
     <Button size={SIZE.compact}>Compact size</Button>
     <Button size={SIZE.large}>Large size</Button>
+    <Button startEnhancer={() => <ArrowRight size={24} />}>
+      Start Enhancer
+    </Button>
+    <Button endEnhancer={() => <Upload size={24} />}>End Enhancer</Button>
   </React.Fragment>
 );
